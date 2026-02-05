ba131e6e55a0c2844d563e63709fa08c64a1ffb9
Ty Conner <tyconner@hotmail.com>
2026-02-04 21:42:46 -0600

    Player and Camera controllers
    Player Controller
    Camera Controller
    Input mappings
    Layers
    Player prefab

---
+++ GDB-Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,125 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] LayerMask ignoreLayer;
+
+    [SerializeField] CharacterController characterController;
+    [SerializeField] CameraController cameraController;
+    [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float sprintModifier = 1.7f;
+    [SerializeField] int HP = 100;
+    [SerializeField] int jumpVelocity = 15;
+    [SerializeField] int jumpMax = 1;
+    [SerializeField] float gravity = 32f;
+
+    [SerializeField] int shootDamage = 20;
+    [SerializeField] int shootDistance = 500;
+    [SerializeField] float shootRate = 0.25f;
+
+    int jumpCount = 0;
+    int startingHP;
+    float startingMovespeed;
+
+    float shootTimer;
+
+    Vector3 moveDir;
+    Vector3 playerVelocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startingHP = HP;
+        startingMovespeed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Sprint();
+
+        shootTimer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDistance, Color.red);
+    }
+
+    void Movement()
+    {
+        if (characterController.isGrounded)
+        {
+            jumpCount = 0;
+            playerVelocity = Vector3.zero; //zero out gravity
+        }
+
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        moveDir = Vector3.ClampMagnitude(moveDir, 1f); //We must clamp or we go faster when pressing two directions at once
+        characterController.Move(moveDir * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButton("Jump"))
+        {
+            Jump();
+        }
+
+        characterController.Move(playerVelocity * Time.deltaTime);
+
+        playerVelocity.y -= gravity * Time.deltaTime;//gravity
+
+
+    }
+
+    void Jump()
+    {
+        if (jumpCount < jumpMax)
+        {
+            playerVelocity.y = jumpVelocity;
+            jumpCount++;
+        }
+    }
+
+    void Sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            moveSpeed *= sprintModifier;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            moveSpeed = startingMovespeed;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        HP -= Mathf.Clamp(amount, 0, startingHP);
+        if (HP <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        print("You died");
+    }
+
+    void Shoot()//Gun script will handle shoot implementation
+    {
+        if (shootTimer >= shootRate) //Gun will handle fire rate
+        {
+            shootTimer = 0;
+
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDistance, ~ignoreLayer))
+            {
+                Debug.Log(hit.collider.name);
+                //TODO: Implement shoot mechanic (this likely will be done in a gun script, not here)
+                // gun.shoot();
+            }
+        }
+    }
+}
