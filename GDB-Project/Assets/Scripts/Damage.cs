ad3c9384fa7f3d50fb555d4f7434b67d3ff8a923
xelectrowolfx <116964236+xelectrowolfx@users.noreply.github.com>
2026-02-12 00:03:21 -0500

    damage update

--- GDB-Project/Assets/Scripts/Damage.cs
+++ GDB-Project/Assets/Scripts/Damage.cs
@@ -21,7 +21,7 @@ public class Damage : MonoBehaviour
     [SerializeField] ParticleSystem hitEff;
 
 
-    PlayerState Creator;
+    public PlayerState Creator;
 
     public void SetOwningPlayer(PlayerState player)
     {
