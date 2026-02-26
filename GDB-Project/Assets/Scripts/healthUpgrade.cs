3beec20ac942e2460edf7eab911b30860772d75a
xelectrowolfx <116964236+xelectrowolfx@users.noreply.github.com>
2026-02-26 05:06:09 -0500

    Barrels Respawn, Bang Barrel Implemented

--- GDB-Project/Assets/Scripts/healthUpgrade.cs
+++ GDB-Project/Assets/Scripts/healthUpgrade.cs
@@ -5,15 +5,36 @@ public class healthUpgrade : MonoBehaviour
 {
     [SerializeField] int healthIncrease = 20;
     public AudioClip healthupgradeSound;
+    [SerializeField] MeshRenderer mesh;
+    [SerializeField] int RespawnTimer = 3;
+    enum state { Interactable, PhasedOut }
+    state mystate = state.Interactable;
+    IEnumerator TimeOut()
+    {
+        mystate = state.PhasedOut;
+        mesh.enabled = false;
+        yield return new WaitForSeconds(RespawnTimer);
+        mystate = state.Interactable;
+        mesh.enabled = true;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        PlayerController player = other.GetComponentInParent<PlayerController>();
-
-        if (player != null)
+        if (other.isTrigger)
         {
-            AudioSource.PlayClipAtPoint(healthupgradeSound, transform.position);
-            player.addHealth(healthIncrease);
-            Destroy(gameObject); 
+            return;
+        }
+        if (mystate == state.Interactable)
+        {
+            iUseWeaponsAndItems player = other.GetComponent<iUseWeaponsAndItems>();
+
+            if (player != null)
+            {
+                {
+                    AudioSource.PlayClipAtPoint(healthupgradeSound, transform.position);
+                    player.addHealth(healthIncrease);
+                    StartCoroutine(TimeOut());
+                }
+            }
         }
     }
 }
